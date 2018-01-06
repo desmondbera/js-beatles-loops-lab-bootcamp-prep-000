8bb1a328f2e595097cb4b71b97ab63a43ae1a8f3
desmond beramendi <desmond7187@gmail.com>
2018-01-06 05:43:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  
+  for(let i = 0; i < musicians.length; i++) {
+    var newString = musicians[i] + " plays " + instruments[i]
+    arr.push(newString);
+  }
+  return arr;
+}
+
+/*
+function johnLennonFacts(array) {
+  
+  var i = 0 
+  while(i < array.length) {
+    array.push("!!!");
+    i++;
+  }
+  return array;
+}
+*/
+
+function johnLennonFacts(array) {
+  var results = []
+  var i = 0 
+  while(i < array.length) {
+    results.push(array[i] + "!!!");
+    // var newFact = array[i] + "!!!"
+    // array.push(newFact);
+    i++
+  }
+  return results; 
+}
