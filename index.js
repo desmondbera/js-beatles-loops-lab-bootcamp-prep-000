ad12818e3dc535fd26a1707b4ea76966bd0ebf57
desmond beramendi <desmond7187@gmail.com>
2018-01-06 06:00:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,8 +35,10 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(num) {
   var emptyArr = []
   
-  do {
-    
+  var i = 0 
+  do { 
+    emptyArr.push("I love the Beatles!")
+    i++
   } while(num < 15 )
   
 }
